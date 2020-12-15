26f29af3429168cccd7a444aa4a96a65aa261e9a
Davi Lucas <davilucasdlcreator@gmail.com>
2020-12-15 20:31:28 -0300

    Animação

--- components/Technology/index.tsx
+++ components/Technology/index.tsx
@@ -16,12 +16,12 @@ const Technology: React.FC<TechnologyProps> = ({ imgSrc, layoutId}) => {
       />
 
       <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ scale: 0,opacity: 0 }}
+        animate={{ rotate: 360, scale: 1,opacity: 1 }}
         transition={{ delay: 1, duration: 1 }}
       >
         <Link href="/">
-          <a className="bg-gray-900 text-gray-50 px-6 py-3 text-lg font-semibold rounded-xl hover:bg-gray-700 transition">
+          <a className="bg-blue-700 text-white px-6 py-3 text-lg font-semibold rounded-xl hover:bg-blue-900 transition">
             Voltar para a Home
           </a>
         </Link>
